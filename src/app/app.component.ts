c523409488c07c1b51675c7c3f0acb02f3eeec1c
code <code.jfe@gmail.com>
2023-07-26 13:25:13 -0500

    html data module management

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -6,5 +6,32 @@ import { Component } from '@angular/core';
   styleUrls: ['./app.component.scss']
 })
 export class AppComponent {
-  title = 'angularApp';
+  nickName = 'CodeMMs';
+  age = 25;
+  img = 'https://encrypted-tbn0.gstatic.com/images?q=tbn:ANd9GcR7HZr81YPvJJ02lxszyYDmiYSeZ9FT7qfMHA&usqp=CAU'
+  btnDisabled = true;
+
+  person = {
+    name: 'juan',
+    age: 25,
+    avatar: 'https://encrypted-tbn0.gstatic.com/images?q=tbn:ANd9GcR7HZr81YPvJJ02lxszyYDmiYSeZ9FT7qfMHA&usqp=CAU'
+  }
+
+  toggleButton() {
+    this.btnDisabled = !this.btnDisabled;
+  }
+
+  ageIncrease() {
+    this.person.age += 1;
+  }
+
+  onScroll(event: Event) {
+    const element = event.target as HTMLElement;
+    console.log(element.scrollTop);
+  }
+
+  changeName(event: Event) {
+    const element = event.target as HTMLInputElement;
+    this.person.name = element.value;
+  }
 }
